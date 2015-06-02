3175186492a163055ed0577443028968ae44423e
Eli Zoller <zoller.eli@gmail.com>
2015-06-02 15:17:35 -0400

    refactoring for import button on settings page - not triggered by save of settings anymore - looks to external scripts to process import

--- drs-tk.php
+++ drs-tk.php
@@ -8,7 +8,9 @@
  */
 
 require_once( plugin_dir_path( __FILE__ ) . 'inc/item.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'inc/import.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'inc/browse.php' );
+
 define( 'ALLOW_UNFILTERED_UPLOADS', true ); //this will allow files without extensions - aka from fedora
 
 $VERSION = '0.1.0';
@@ -47,103 +49,6 @@ $TEMPLATE = array(
      flush_rewrite_rules();
  }
 
- function drstk_get_media_images($collection_pid) {
-   //echo $collection_pid;
-
-   //first we get the existing images
-   $query_images_args = array(
-      'post_type' => 'attachment', 'post_mime_type' =>'image', 'post_status' => 'inherit', 'posts_per_page' => -1,
-    );
-   $query_images = new WP_Query( $query_images_args );
-   $images = array();
-   foreach ( $query_images->posts as $image) {
-      $images[]= basename(wp_get_attachment_url( $image->ID ));
-   }
-    //print_r($images);
-    //get all the core_files from the drs
-    $drs_url = "http://cerberus.library.northeastern.edu/api/v1/search/".$collection_pid."?per_page=20";
-    //will there be some kind of helper with the api to get them all back at once? don't want to waste time writing a function that would loop through the pages of results
-    $json = get_response($drs_url);
-    //print_r($json);
-    $json = json_decode($json);
-    if ($json->response->response->numFound > 0) {
-      foreach($json->response->response->docs as $doc) {
-        if ($doc->active_fedora_model_ssi == "CoreFile") {
-          $title = $doc->title_ssi;
-          //if its an image just send the master image
-          //if its not an image send a thumbnail
-          //assign $title, $creator, $date, $description
-          $creator = "Jim Bob";
-          $date = " ";
-          $description = "best description eveer";
-          $url = "http://cerberus.library.northeastern.edu" . end($doc->fields_thumbnail_list_tesim);
-          //$url = str_replace("thumbnail_1","content", $url);
-          echo $url;
-          drstk_process_image($url, $images, $title, $creator, $date, $description);
-        }
-      }
-    }
-  }
-
-  function drstk_process_image($url, $images, $title, $creator, $date, $description){
-    $pid = explode("/", $url);
-    $pid = explode("?", end($pid));
-    $pid = str_replace(":","",$pid[0]);
-    echo $pid;
-    if (!in_array($pid, $images)){
-      $tmp = download_url( $url );
-      $post_id = 0;
-      $file_array = array();
-
-      // Set variables for storage
-      $file_array['name'] = $pid;
-      $file_array['type'] = 'image/jpeg';
-      $file_array['error'] = 0;
-      $file_array['tmp_name'] = $tmp;
-      $file_array['size'] = filesize($tmp);
-      //print_r($file_array);
-
-      // If error storing temporarily, unlink
-      if ( is_wp_error( $tmp ) ) {
-        @unlink($file_array['tmp_name']);
-        $file_array['tmp_name'] = '';
-      }
-
-      // do the validation and storage stuff
-      $id = media_handle_sideload( $file_array, 0);
-      echo $id . "<br/>";
-
-      // If error storing permanently, unlink
-      if ( is_wp_error($id) ) {
-        @unlink($file_array['tmp_name']);
-        return $id;
-      }
-
-      $src = wp_get_attachment_url( $id );
-      $image_id = drstk_get_image_id($src);
-      //permalink redirect from image url to item level page?
-      echo $src . "<br/>";
-      echo $image_id . "<br/>";
-      $image_title = "This is a fake image title to test function";//need to pull this from API
-      $image_description = "This is a fake image description to test functionality.";//need to pull this from API
-      $image_metadata = "some awesome metadata here";//need to pull an array of metadata from the API
-      $image_post = array(
-        'ID' => $image_id,
-        'post_title' => $image_title,
-        'post_excerpt' => $image_description,
-      );
-      wp_update_post($image_post);
-      update_post_meta($image_id, 'drstk-drs-metadata', $image_metadata);
-    }
-  }
-
-  function drstk_get_image_id($image_url) {
-  	global $wpdb;
-  	$attachment = $wpdb->get_col($wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE guid='%s';", $image_url ));
-          return $attachment[0];
-  }
-
-
 //This function creates the settings page for entering the pid
  add_action('admin_menu', 'drs_admin_add_page');
  function drs_admin_add_page() {
@@ -157,10 +62,6 @@ $TEMPLATE = array(
      {
         //plugin settings have been saved.
         $collection_pid = get_option('drstk_collection');
-        $sync_images = get_option('drstk_sync_images');
-        if ($sync_images == true) {
-          drstk_get_media_images($collection_pid);
-        }
      }
   }
 
@@ -168,8 +69,6 @@ $TEMPLATE = array(
  function drstk_display_settings() {
 
      $collection_pid = (get_option('drstk_collection') != '') ? get_option('drstk_collection') : 'neu:1';
-     $sync_images = get_option('drstk_sync_images');
-
      $html = '</pre>
      <div class="wrap">
      <form action="options.php" method="post" name="options">
@@ -186,16 +85,7 @@ $TEMPLATE = array(
      </td>
      </tr>
      <tr>
-     <td scope="row" align="left">
-     <label>Sync Images</label>
-     <input type="checkbox" value="true" name="drstk_sync_images" ';
-     if ($sync_images == true) {
-       $html .= 'checked="checked"';
-     }
-     $html .= ' ></input>
-     <br/>
-     <small>If you would like to sync images from the DRS, check the box.</small>
-     </td>
+     <td><a class="button" id="drstk-import" href="#">Import Items from the DRS</a></td>
      </tr>
      </tbody>
      </table>
@@ -210,6 +100,26 @@ $TEMPLATE = array(
      echo $html;
 
  }
+
+ function drstk_admin_enqueue() {
+    if (get_current_screen()->base == 'settings_page_drstk_admin_menu') {
+      //we are on the settings page
+      wp_enqueue_script( 'drstk_import',
+          plugins_url( '/assets/js/import.js', __FILE__ ),
+          array( 'jquery' )
+      );
+      //this creates a unique nonce to pass back and forth from js/php to protect
+      $import_nonce = wp_create_nonce( 'import_drs' );
+      //this allows an ajax call from import.js
+      wp_localize_script( 'drstk_import', 'import_obj', array(
+         'ajax_url' => admin_url( 'admin-ajax.php' ),
+         'nonce'    => $import_nonce,
+         'pid' => get_option('drstk_collection'),
+      ) );
+
+    }
+}
+add_action('admin_enqueue_scripts', 'drstk_admin_enqueue');
 
  /**
   * Register an additional query variable so we can differentiate between
