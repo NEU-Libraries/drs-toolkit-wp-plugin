a83484379f14ff254036d3fbc3d3d40c4167dc56
Eli Zoller <zoller.eli@gmail.com>
2015-06-04 13:44:04 -0400

    modifying template for minamaze

--- templates/browse.php
+++ templates/browse.php
@@ -3,38 +3,30 @@
  * This is just a temporary placeholder template to demonstrate that it is working.
  * You will want to setup your own template for your theme to display the information.
  */
-
 get_header(); ?>
 <noscript>
 <div class="noscript_warning">PLEASE NOTE: JAVASCRIPT IS DISABLED ON YOUR BROWSER. For the best user experience, please enable javascript on your browser now.</div>
 </noscript>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-
-
 <div id="drs-content">
-  <div id="drs-browse-header-row">
-    <h2 id="drs-pagination-header"></h2>
-    <ul id="drs-pagination"></ul>
-    <div id="drs-search"><!-- class="hidden" this class will depend on theme-->
-      <label for="drs-input" class="sr-only">Search:</label>
-      <input id="drs-input" name="drs-input" autocomplete="off" type="text" placeholder="Enter search term">
-			<input type="submit" value="Search" />
-    </div>
+	<div id="drs-search"><!-- class="hidden" this class will depend on theme-->
+		<label for="drs-input" class="sr-only">Search:</label>
+		<input id="drs-input" name="drs-input" autocomplete="off" type="text" placeholder="Enter search term">
+		<input type="submit" value="Search" />
+	</div>
+  <div id="drs-browse-header-row" class="pagination">
+    <h4>
+			<div id="drs-pagination-header"></div>
+			<div id="drs-sort"></div>
+    	<ul id="drs-pagination"></ul>
+		</h4>
   </div><!-- #drs-browse-header-row -->
-	<div id="drs-sort">Sort By: </div>
   <ul id="drs-selection"></ul>
-	<div id="drs-facets" class="panel panel-default grid-25"></div>
-  <div id="drs-docs" class="grid-75">
+	<div id="drs-facets" class="one_fourth"></div>
+  <div id="drs-docs" class="three_fourth">
     <!-- <div id="drs-docs-even" class="col-sm-12 col-md-6"></div>
     <div id="drs-docs-odd" class="col-sm-12 col-md-6"></div> -->
   </div>
 </div><!-- #drs-content -->
-
-
-</main><!-- .site-main -->
-</div><!-- .content-area -->
 
 <?php get_footer(); ?>
