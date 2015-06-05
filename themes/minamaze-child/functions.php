87d1cdf1638599adc846d41fc704cc9abe272b80
Eli Zoller <zoller.eli@gmail.com>
2015-06-05 08:58:30 -0400

    overriding parent theme functions to accomodate for our templates page titles

--- themes/minamaze-child/functions.php
+++ themes/minamaze-child/functions.php
@@ -22,3 +22,63 @@ function my_search_form( $form ) {
 }
 
 add_filter( 'get_search_form', 'my_search_form' );
+
+
+/*COPIED FROM minamaze/admin/main/options/01.general-settings.php*/
+/* Add custom intro section [Extend for more options in future update] */
+function drs_custom_intro() {
+
+	if ( ! is_front_page() ) {
+		echo	'<div id="intro" class="option1"><div id="intro-core">',
+				'<h1 class="page-title"><span>',
+				drs_title_select(),
+				'</span></h1>',
+				thinkup_input_breadcrumbswitch(),
+				'</div></div>';
+	} else {
+		echo '';
+	}
+}
+
+/*COPIED FROM minamaze/admin/main/options/01.general-settings.php*/
+function drs_title_select() {
+	global $post;
+  global $wp_query;
+  $template_type = $wp_query->query_vars['drstk_template_type'];
+
+  if ($template_type == 'search'){
+    printf( __('Search', 'lan-thinkupthemes'));
+  } elseif ($template_type == 'browse'){
+    printf( __('Browse', 'lan-thinkupthemes'));
+  } elseif ($template_type == 'item'){
+    return;
+  } elseif ( is_page() ) {
+		printf( __( '%s', 'lan-thinkupthemes' ), get_the_title() );
+	} elseif ( is_attachment() ) {
+		printf( __( 'Blog Post Image: %s', 'lan-thinkupthemes' ), esc_attr( get_the_title( $post->post_parent ) ) );
+	} else if ( is_single() ) {
+		printf( __( '%s', 'lan-thinkupthemes' ), get_the_title() );
+	} else if ( is_search() ) {
+		printf( __( 'Search Results: %s', 'lan-thinkupthemes' ), get_search_query() );
+	} else if ( is_404() ) {
+		printf( __( 'Page Not Found', 'lan-thinkupthemes' ) );
+	} else if ( is_category() ) {
+		printf( __( 'Category Archives: %s', 'lan-thinkupthemes' ), single_cat_title( '', false ) );
+	} elseif ( is_tag() ) {
+		printf( __( 'Tag Archives: %s', 'lan-thinkupthemes' ), single_tag_title( '', false ) );
+	} elseif ( is_author() ) {
+		the_post();
+		printf( __( 'Author Archives: %s', 'lan-thinkupthemes' ), get_the_author() );
+		rewind_posts();
+	} elseif ( is_day() ) {
+		printf( __( 'Daily Archives: %s', 'lan-thinkupthemes' ), get_the_date() );
+	} elseif ( is_month() ) {
+		printf( __( 'Monthly Archives: %s', 'lan-thinkupthemes' ), get_the_date( 'F Y' ) );
+	} elseif ( is_year() ) {
+		printf( __( 'Yearly Archives: %s', 'lan-thinkupthemes' ), get_the_date( 'Y' ) );
+	} elseif ( thinkup_is_blog() ) {
+		printf( __( 'Blog', 'lan-thinkupthemes' ) );
+	} else {
+		printf( __( '%s', 'lan-thinkupthemes' ), get_the_title() );
+	}
+}
