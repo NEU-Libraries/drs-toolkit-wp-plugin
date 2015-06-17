3eb6542209d3a63b5bea12c090d641a0afdd0d90
Eli Zoller <zoller.eli@gmail.com>
2015-06-17 11:54:55 -0400

    building jwplayer if object is video or audio

--- plugins/drs-tk/assets/js/item.js
+++ plugins/drs-tk/assets/js/item.js
@@ -27,6 +27,45 @@ jQuery(document).ready(function($) {
     if (data.thumbnails) {
       $("#drs-item-img").attr("src",data.thumbnails[data.thumbnails.length - 1]);
     }
+    if (data.canonical_object[0][1] == 'Video File' || data.canonical_object[0][1] == 'Audio File'){
+      $("#drs-item-img").after("<div id='drs-item-video'></div>").hide();
+        jwplayer.key="gi5wgpwDtAXG4xdj1uuW/NyMsECyiATOBxEO7A=="
+        var provider = data.av_provider;
+        var type = data.av_type;
+        var encoded = data.encoded_av_pid;
+        var dir = data.av_dir;
+        var poster = data.av_poster;
+        var primary = "flash"
+        if (typeof swfobject == 'undefined' || swfobject.getFlashPlayerVersion().major == 0) {
+          primary == "html5"
+        }
+        jwplayer("drs-item-video").setup({
+        sources:
+        [
+        { file: "rtmp://libwowza.neu.edu:1935/vod/_definst_/"+type+":datastreamStore/cerberusData/newfedoradata/datastreamStore/"+dir+"/info%3Afedora%2F"+encoded+"%2Fcontent%2Fcontent.0"},
+        { file: "http://libwowza.neu.edu:1935/vod/_definst_/datastreamStore/cerberusData/newfedoradata/datastreamStore/"+dir+"/"+type+":" + encodeURIComponent("info%3Afedora%2F"+encoded+"%2Fcontent%2Fcontent.0") + "/playlist.m3u8", type:type}
+        ],
+        image: poster,
+        provider: provider,
+        fallback: "true",
+        androidhls: "true",
+        primary: primary,
+        width: "100%",
+        height: 400,
+        })
+        var errorMessage = function() {
+          $("#drs-item-img").show()
+          $("#drs-item-video").hide()
+        }
+        jwplayer().onError(errorMessage);
+        jwplayer().onSetupError(errorMessage);
+        jwplayer().onBuffer(function() {
+          theTimeout = setTimeout(function() {
+            $("#drs-item-img").show()
+            $("#drs-item-video").hide()
+          }, 5000)
+        })
+    }
     var data_html = '';
     $.each(data.mods, function(key,value){
       data_html += "<div><b>"+key+"</b></div><div>"+value+"</div>";
