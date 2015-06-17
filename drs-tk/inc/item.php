15a9d8fabac9599097092d6c21e9394ae74ffc67
Eli Zoller <zoller.eli@gmail.com>
2015-06-17 11:54:37 -0400

    appending data for building jwplayer to returned item json

--- drs-tk/inc/item.php
+++ drs-tk/inc/item.php
@@ -9,5 +9,29 @@ function item_ajax_handler() {
       $url .= $_POST['pid'];
     }
     $data = get_response($url);
-    wp_send_json($data);
+    $data = json_decode($data, true);
+    if ($data['canonical_object'][0][1] == 'Video File' || $data['canonical_object'][0][1] == 'Audio File'){
+      $data['av_pid'] = $data['canonical_object'][0][0];
+      $data['av_pid'] = explode("/", $data['av_pid']);
+      $data['av_pid'] = end($data['av_pid']);
+      $data['encoded_av_pid'] = str_replace(':','%3A', $data['av_pid']);
+      $data['av_dir'] = substr(md5("info:fedora/".$data['av_pid']."/content/content.0"), 0, 2);
+      $data['av_type'] = $data['canonical_object'][0][0];
+      foreach ($data['content_objects'] as $key=>$val){
+        if ($val[1] == 'Master Image'){
+          $data['av_poster'] = $val[0];
+        }
+      }
+      if ($data['canonical_object'][0][1] == 'Video File'){
+        $data['av_provider'] = 'video';
+        $data['av_type'] = "MP4";
+      }
+      if ($data['canonical_object'][0][1] == 'Audio File'){
+        $data['av_provider'] = 'sound';
+        $data['av_type'] = "MP3";
+      }
+    }
+
+
+    wp_send_json(json_encode($data));
 }
