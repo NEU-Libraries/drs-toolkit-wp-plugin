c36318ebbc80d64c9ec2f17990cb305bef38c1ce
Eli Zoller <zoller.eli@gmail.com>
2015-12-02 12:55:24 -0500

    replacing yaml with php array because yaml involved a php extension

---
+++ inc/errors.php
@@ -0,0 +1,21 @@
+<?php
+  global $errors;
+  $errors = [
+    "admin" => [
+      "api_fail" => "Sorry, DRS files and metadata are currently unavailable. Please refresh the page or try again later. If problem persists please contact dsg@neu.edu.",
+    ],
+    "search" => [
+      "no_results" => "Your query produced no results. Please refine your search and try again.",
+      "fail_null" => "Sorry, these project materials are currently unavailable. Please try again later.",
+      "no_sub_collections" => "This project has no sub-collections.",
+      "missing_collection" => "No collections are available at this time. Please contact the site administrator.",
+    ],
+    "item" => [
+      "no_results" => "This file is currently unavailable. Please check the URL and try again.",
+      "fail" => "Sorry, project materials are currently unavailable. Please refresh the page or try again later. If problem persists please contact the site administrator.",
+      "jwplayer_fail" => "There was an issue playing this file. Please contact the site administrator.",
+    ],
+    "shortcodes" => [
+      "fail" => "Sorry, project materials are currently unavailable. Please refresh the page or try again later. If problem persists please contact the site administrator.",
+    ],
+  ];
