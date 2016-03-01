3c3a02c2ccfa1b8cb62ad05c754eb22ada2b099f
Eli Zoller <zoller.eli@gmail.com>
2016-03-01 13:38:46 -0500

    adding js for admin facets settings and drying out

--- assets/js/item_meta_helper.js
+++ assets/js/item_meta_helper.js
@@ -1,27 +1,37 @@
 jQuery(document).ready(function(){
   jQuery("input[name='drstk_item_metadata']").on('change', function(){
-    var item_meta = [];
-    jQuery("input[name='drstk_item_metadata']").each(function(){
-      if (jQuery(this).is(":checked")){
-        item_meta.push(jQuery(this).val());
-      }
-    })
-    jQuery("input[name='drstk_item_page_metadata']").val(item_meta);
+    compile_val("input[name='drstk_item_metadata']", "input[name='drstk_item_page_metadata']");
   });
   jQuery("input[name='drstk_assoc_metadata']").on('change', function(){
-    var assoc_meta = [];
-    jQuery("input[name='drstk_assoc_metadata']").each(function(){
-      if (jQuery(this).is(":checked")){
-        assoc_meta.push(jQuery(this).val());
-      }
-    })
-    jQuery("input[name='drstk_assoc_file_metadata']").val(assoc_meta);
+    compile_val("input[name='drstk_assoc_metadata']", "input[name='drstk_assoc_file_metadata']");
   });
   jQuery("input[name='drstk_assoc']").on('change', function(){
     if (jQuery(this).is(":checked")){
-      jQuery("tbody.assoc").css("display","table");
+      jQuery("tr.assoc").css("display","table-row");
     } else {
-      jQuery("tbody.assoc").css("display","none");
+      jQuery("tr.assoc").css("display","none");
     }
-  })
+  });
+  jQuery("input[name='drstk_facet']").on('change', function(){
+    compile_val("input[name='drstk_facet']", "input[name='drstk_facets']");
+    if (jQuery(this).is(":checked")){
+      jQuery(this).parents("td").next(".title").css("display","table-cell");
+    } else {
+      jQuery(this).parents("td").next(".title").css("display","none");
+    }
+  });
+  jQuery("#facets_sortable").sortable();
+  jQuery("#facets_sortable").on( "sortstop", function( event, ui ) {
+    compile_val("input[name='drstk_facet']", "input[name='drstk_facets']");
+  });
+
+  function compile_val(ind, agg){
+    var array = [];
+    jQuery(ind).each(function(){
+      if (jQuery(this).is(":checked")){
+        array.push(jQuery(this).val());
+      }
+    });
+    jQuery(agg).val(array);
+  }
 });
