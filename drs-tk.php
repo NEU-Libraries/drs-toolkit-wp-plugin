366190d9d0aa36e5e2921d1584e2ee4cdb9da9fe
Damon Timm <damontimm@gmail.com>
2016-03-07 19:48:20 -0800

    add drstk_home_url setting

--- drs-tk.php
+++ drs-tk.php
@@ -92,6 +92,8 @@ function register_drs_settings() {
   add_settings_section('drstk_project', "Project Info", null, 'drstk_options');
   add_settings_field('drstk_collection', 'Project Collection or Set URL', 'drstk_collection_callback', 'drstk_options', 'drstk_project');
   register_setting( 'drstk_options', 'drstk_collection' );
+  add_settings_field('drstk_home_url', 'Permalink/URL Base', 'drstk_home_url_callback', 'drstk_options', 'drstk_project');
+  register_setting( 'drstk_options', 'drstk_home_url', 'drstk_home_url_validation' );
 
   add_settings_section('drstk_search_settings', 'Search Settings', null, 'drstk_options');
   add_settings_field('drstk_search_page_title', 'Search Page Title', 'drstk_search_page_title_callback', 'drstk_options', 'drstk_search_settings');
@@ -187,6 +189,33 @@ function drstk_collection_callback(){
   $collection_pid = (get_option('drstk_collection') != '') ? get_option('drstk_collection') : 'https://repository.library.northeastern.edu/collections/neu:1';
   echo '<input name="drstk_collection" type="text" value="'.$collection_pid.'" style="width:100%;"></input><br/>
      <small>Ie. <a href="https://repository.library.northeastern.edu/collections/neu:6012">https://repository.library.northeastern.edu/collections/neu:6012</a></small>';
+}
+
+function drstk_home_url_callback() {
+  $url_base = get_option('drstk_home_url');
+  echo '<input name="drstk_home_url" type="text" value="'.$url_base.'"></input><br/>
+     <small>This sets the URL permalink base for /browse/, /search/, /item/, and /collection/<br/>
+     Currently, yours will look like: <strong>'.drstk_home_url().'browse/</strong></small>';
+}
+
+/**
+ * Basic validation and standardization of the $url_base entry;
+ * should return a safe string that ends with a forward slash
+ * (and does not begin with one).
+ */
+function drstk_home_url_validation($input){
+  $url_base = '';
+  $parts = explode("/", $input);
+  foreach ($parts as $part) {
+    if ($part != '') {
+      $safe_part = sanitize_title($part);
+      if ($safe_part) {
+        $url_base .= sanitize_title($part);
+        $url_base .= '/';
+      }
+    }
+  }
+  return $url_base;
 }
 
 function drstk_search_page_title_callback(){
