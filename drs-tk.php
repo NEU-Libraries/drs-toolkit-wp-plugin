2b5933aee40443a7cb999ae86e2b26cea2ecb4b1
Eli Zoller <zoller.eli@gmail.com>
2016-03-15 09:58:38 -0400

    reformatting

--- drs-tk.php
+++ drs-tk.php
@@ -498,15 +498,11 @@ function drstk_item_script() {
     //this enqueues the JS file
     wp_register_script('drstk_jwplayer', plugins_url('/assets/js/jwplayer/jwplayer.js', __FILE__), array(), $VERSION, false );
     wp_enqueue_script('drstk_jwplayer');
-    wp_register_script('drstk_elevatezoom',
-        plugins_url('/assets/js/elevatezoom/jquery.elevateZoom-3.0.8.min.js', __FILE__),
-        array());
+    wp_register_script('drstk_elevatezoom',plugins_url('/assets/js/elevatezoom/jquery.elevateZoom-3.0.8.min.js', __FILE__), array());
     wp_enqueue_script('drstk_elevatezoom');
     wp_register_script('swfobject', '');
     wp_enqueue_script('swfobject');
-    wp_register_script('drstk_item_gallery',
-        plugins_url('/assets/js/item_gallery.js', __FILE__),
-        array(), $VERSION, false );
+    wp_register_script('drstk_item_gallery', plugins_url('/assets/js/item_gallery.js', __FILE__), array(), $VERSION, false );
     wp_enqueue_script('drstk_item_gallery');
 }
 
