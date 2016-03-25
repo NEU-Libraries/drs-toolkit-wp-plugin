2ae1135b9cf62631e1bd47d0e37662d4dd65239c
sritharan mahendra babu <srithar@ccs.neu.edu>
2016-03-25 11:54:57 -0400

    Timeline revert

--- assets/js/timelinepage.js
+++ assets/js/timelinepage.js
@@ -1,15 +1,15 @@
 jQuery(document).ready(function($) {
 
-    var eventsList = getItemsFromJqueryArrayTimelineArray($('.timelineclass'));
-
-    // $('.timelineclass').each(function(index) {
-    //     var timelineClass = '.timelineclass';
-    //     eventsList.push({
-    //         media: {url : genericRetrieval(index, timelineClass, 'url'), caption:genericRetrieval(index, timelineClass, 'caption'), credit:genericRetrieval(index, timelineClass, 'capiton')},
-	// 		start_date: {year:genericRetrieval(index, timelineClass, 'year'), month:genericRetrieval(index, timelineClass, 'month'), day:genericRetrieval(index, timelineClass, 'day')},
-	// 		text: {headline:genericRetrieval(index, timelineClass, 'headline'), text:genericRetrieval(index, timelineClass, 'text')}
-    //     });
-    // });
+    // var eventsList = getItemsFromJqueryArrayTimelineArray($('.timelineclass'));
+    var eventsList = [];
+    $('.timelineclass').each(function(index) {
+        var timelineClass = '.timelineclass';
+        eventsList.push({
+            media: {url : genericRetrieval(index, timelineClass, 'url'), caption:genericRetrieval(index, timelineClass, 'caption'), credit:genericRetrieval(index, timelineClass, 'capiton')},
+			start_date: {year:genericRetrieval(index, timelineClass, 'year'), month:genericRetrieval(index, timelineClass, 'month'), day:genericRetrieval(index, timelineClass, 'day')},
+			text: {headline:genericRetrieval(index, timelineClass, 'headline'), text:genericRetrieval(index, timelineClass, 'text')}
+        });
+    });
     
     var finalTimelineJson = {events:eventsList};
     // console.log(finalTimelineJson);
