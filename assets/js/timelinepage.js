c61aa5ebd47af26aff704561ac7d2aae494cc230
Sritharan <srithar@ccs.neu.edu>
2016-03-24 17:52:12 -0700

    Working feature of timeline

--- assets/js/timelinepage.js
+++ assets/js/timelinepage.js
@@ -1,4 +1,17 @@
 jQuery(document).ready(function($) {
 
-   console.log("Entering Timeline Mode");
-});//end doc ready
+    var eventsList = [];
+
+    $('.timelineclass').each(function(index) {
+
+        eventsList.push({
+            media: {url : $($('.timelineclass')[index]).data('url'), caption:$($('.timelineclass')[index]).data('caption'), credit:$($('.timelineclass')[index]).data('credit')},
+			start_date: {year:$($('.timelineclass')[index]).data('year'), month:$($('.timelineclass')[index]).data('month'), day:$($('.timelineclass')[index]).data('day')},
+			text: {headline:$($('.timelineclass')[index]).data('headline'), text:$($('.timelineclass')[index]).data('text')}
+        });
+    });
+    
+    var finalTimelineJson = {events:eventsList};
+    console.log(finalTimelineJson);
+    window.timeline = new TL.Timeline('timeline-embed', finalTimelineJson);
+});
