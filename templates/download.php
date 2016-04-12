bd284ef19ce3c091e7f55314a148f4ba1a5d4f97
Eli Zoller <zoller.eli@gmail.com>
2016-04-12 07:52:28 -0400

    for #88 - add buffering because some of the video files are so big

--- templates/download.php
+++ templates/download.php
@@ -10,5 +10,11 @@ if (isset($item_pid)){
   $av_dir = substr(md5("info:fedora/".$av_pid."/content/content.0"), 0, 2);
   $full_pid = "info%3Afedora%2F".$encoded_av_pid."%2Fcontent%2Fcontent.0";
   $url = "http://libwowza.neu.edu/datastreamStore/cerberusData/newfedoradata/datastreamStore/".$av_dir."/".urlencode($full_pid);
-  echo file_get_contents($url);
+  $handle = @fopen($url, "r");
+  if ($handle) {
+      while (($buffer = fgets($handle, 4096)) !== false) {
+          echo $buffer;
+      }
+      fclose($handle);
+  }
 }
