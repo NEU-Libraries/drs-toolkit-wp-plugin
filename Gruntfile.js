39266f002e2ccba80103cfc09d44c1ccb14cc93b
Eli Zoller <zoller.eli@gmail.com>
2016-05-03 13:46:43 -0400

    trying to get jasmine + coveralls to work together

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,12 +11,39 @@ module.exports = function(grunt) {
                     'node_modules/jasmine-jquery/lib/jasmine-jquery.js'
                 ],
                 specs : 'specs/**/*.js',
-                junit: {
-                    path: './specs/results/',
-                    consolidate: true
-                }
-            }
+            		templateOptions: {
+            			coverage: "./specs/reports/coverage.json",
+            			report: [
+            				{
+            					type: "html",
+            					options: {
+            						dir: "./specs/reports/html"
+            					}
+            				},
+            				{
+            					type: "lcov",
+            					options: {
+            						dir: "./specs/reports/lcov"
+            					}
+            				},
+            				]
+            			},
+        junit: {
+            path: './specs/results/',
+            consolidate: true
         }
-    });
+      }
+  },
+	coveralls: {
+		options: {
+			// dont fail if coveralls fails
+			force: true
+		},
+		main_target: {
+			src: "./specs/reports/lcov/lcov.info"
+		}
+	},
+});
     grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-coveralls');
 };
