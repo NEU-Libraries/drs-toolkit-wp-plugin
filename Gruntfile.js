fcd1fa904fcd3ec5b7da72902c133fb0c06174cc
Eli Zoller <zoller.eli@gmail.com>
2016-05-04 07:22:08 -0400

    trying to get coveralls to read selenium and jasmine tests and cleaning up gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,56 +2,41 @@ module.exports = function(grunt) {
     'use strict';
     // Project configuration.
     grunt.initConfig({
-        // jasmine : {
-        //     src : 'assets/**/*.js',
-        //     options : {
-        //         vendor: [
-        //             'node_modules/jquery/dist/jquery.js',
-        //             'assets/js/jwplayer/jwplayer.js',
-        //             'node_modules/jasmine-jquery/lib/jasmine-jquery.js'
-        //         ],
-        //         specs : 'specs/**/*.js',
-        //         junit: {
-        //             path: './specs/results/',
-        //             consolidate: true
-        //         }
-        //     }
-        // }
         jasmine: {
-  pivotal: {
-    src: "assets/**/*.js",
-    options: {
-      specs: "specs/**/*.js",
-      junit: {
-                  path: './specs/results/',
-                  consolidate: true
-              },
-      template : require("grunt-template-jasmine-istanbul"),
-      templateOptions: {
-        coverage: "reports/coverage.json",
-        report: [
-          {
-            type: "html",
-            options: {
-              dir: "reports/html"
-            }
-          },
-          {
-            type: "lcov",
-            options: {
-              dir: "reports/lcov"
-            }
-          },
+    pivotal: {
+      src: "assets/**/*.js",
+      options: {
+        specs: "specs/**/*.js",
+        junit: {
+                    path: './specs/results/',
+                    consolidate: true
+                },
+        template : require("grunt-template-jasmine-istanbul"),
+        templateOptions: {
+          coverage: "reports/coverage.json",
+          report: [
+            {
+              type: "html",
+              options: {
+                dir: "reports/html"
+              }
+            },
+            {
+              type: "lcov",
+              options: {
+                dir: "reports/lcov"
+              }
+            },
+          ]
+        },
+        vendor: [
+          'node_modules/jquery/dist/jquery.js',
+          'assets/js/jwplayer/jwplayer.js',
+          'node_modules/jasmine-jquery/lib/jasmine-jquery.js'
         ]
-      },
-      vendor: [
-        'node_modules/jquery/dist/jquery.js',
-        'assets/js/jwplayer/jwplayer.js',
-        'node_modules/jasmine-jquery/lib/jasmine-jquery.js'
-      ]
+      }
     }
-  }
-},
+  },
   coveralls: {
   options: {
 
