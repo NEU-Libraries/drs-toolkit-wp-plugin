8f89cef37b81c6081bdbc0a1fbf6c91096037b67
aravindc16 <aravindc16@ccs.neu.edu>
2016-11-28 04:06:53 -0500

    LIB1-45, LIB1-46, LIB1-58, LIB1-60 Sprint 3 Changes

--- inc/timeline_shortcode.php
+++ inc/timeline_shortcode.php
@@ -1,10 +1,50 @@
 <?php
+
+add_action( 'wp_ajax_reload_filtered_set_timeline', 'reload_filtered_set_timeline_ajax_handler' ); //for auth users
+add_action( 'wp_ajax_nopriv_reload_filtered_set_timeline', 'reload_filtered_set_timeline_ajax_handler' ); //for nonauth users
+function reload_filtered_set_timeline_ajax_handler()
+{   $test = get_response("https://repository.library.northeastern.edu/api/v1/search/neu:cj82kp79t");
+    $test = json_decode($test);
+    if ($_POST['reloadWhat'] == "timelineReload") {
+        echo drstk_timeline($_POST['atts'], $_POST['params']);
+    }
+    else if($_POST['reloadWhat'] == "facetReload") {
+        if (isset($_POST['atts']['collection_id'])) {
+            $url = "https://repository.library.northeastern.edu/api/v1/search/neu:cj82kp79t?per_page=".count($test->pagination->table->total_count);
+            if (isset($_POST['params']['f'])) {
+                foreach ($_POST['params']['f'] as $facet => $facet_val) {
+                    $url .= "&f[" . $facet . "][]=" . urlencode($facet_val);
+                }
+            }
+            if (isset($_POST['params']['q']) && $_POST['params']['q'] != ''){
+                $url .= "&q=". urlencode(sanitize_text_field($_POST['params']['q']));
+            }
+            $data1 = get_response($url);
+            $data1 = json_decode($data1);
+            $facets_info_data = $data1;
+            wp_send_json($facets_info_data);
+        }
+    }
+    die();
+}
+
+
+add_action( 'wp_ajax_reloadRemainingTimeline', 'reloadRemainingTimeline_ajax_handler' ); //for auth users
+add_action( 'wp_ajax_nopriv_reloadRemainingTimeline', 'reloadRemainingTimeline_ajax_handler' ); //for nonauth users
+function reloadRemainingTimeline_ajax_handler()
+{
+    $a = get_post($_POST['post_id'])->post_content;
+    $parsed_a = shortcode_parse_atts($a);
+    /*write_log($parsed_a['collection_id']);*/
+    echo drstk_timeline($parsed_a, $_POST['page_no']);
+    die();
+}
+
 /* adds shortcode */
 
 add_shortcode( 'drstk_timeline', 'drstk_timeline' );
-function drstk_timeline( $atts ){
+function drstk_timeline( $atts, $params ){
   global $errors;
-  global $main_data;
 
   $cache = get_transient(md5('PREFIX'.serialize($atts)));
 
@@ -54,32 +94,49 @@ function drstk_timeline( $atts ){
 
     $collectionItemsId = array();
 
+    $facets_info_data = array();
+
+    $test = get_response("https://repository.library.northeastern.edu/api/v1/search/neu:cj82kp79t");
+    $test = json_decode($test);
+
     if(isset($atts['collection_id'])){
 
-        $data1 = get_response("https://repository.library.northeastern.edu/api/v1/search/neu:cj82kp79t?per_page=20");
-        $data1 = json_decode($data1);
-        $test = $data1;
-        $num_pages = $data1->pagination->table->num_pages;
-        $counter = 1;
-        while($counter <= $num_pages){
-            $url_local = "https://repository.library.northeastern.edu/api/v1/search/neu:cj82kp79t?per_page=20&page=".$counter."";
-            $data2 = get_response($url_local);
-            $data2 = json_decode($data2);
-            $docs2 = $data2->response->response->docs;
-            $counter = $counter + 1;
 
-            foreach($docs2 as $docItem){
-                $collectionItemsId [] = $docItem->id;
+        $url = "https://repository.library.northeastern.edu/api/v1/search/neu:cj82kp79t?per_page=".$test->pagination->table->total_count; //?per_page=10
+
+//        if(isset($params['page_no'])){
+//            $url .= "&page=" . $params['page_no'];
+//        }
+
+        if (isset($params['f'])) {
+            foreach ($params['f'] as $facet => $facet_val) {
+                $url .= "&f[" . $facet . "][]=" . urlencode($facet_val);
             }
         }
+
+        if (isset($params['q']) && $params['q'] != ''){
+            $url .= "&q=". urlencode(sanitize_text_field($params['q']));
+        }
+
+       // write_log($url);
+        $data1 = get_response($url);
+        $data1 = json_decode($data1);
+        $facets_info_data = $data1;
+        $num_pages = $data1->pagination->table->num_pages;
+
+
+        if(isset($params['page_no']) && $params['page_no'] > $num_pages){
+            return "All_Pages_Loaded";
+        }
+
+        $docs2 = $data1->response->response->docs;
+
+        foreach($docs2 as $docItem){
+            $collectionItemsId [] = $docItem->id;
+        }
+
         $neu_ids = $collectionItemsId;
     }
-    $data0 = get_response("https://repository.library.northeastern.edu/api/v1/search/neu:cj82kp79t?per_page=20");
-    $data0 = json_decode($data0);
-    $main_data = array(
-        'ajax_url' => admin_url('admin-ajax.php'),
-        'data' => $data0
-    );
 
 
   foreach($neu_ids as $current_key => $neu_id){
@@ -87,7 +144,7 @@ function drstk_timeline( $atts ){
     $repo = drstk_get_repo_from_pid($neu_id);
     if ($repo != "drs"){$pid = explode(":",$neu_id); $pid = $pid[1];} else {$pid = $neu_id;}
     if($repo == "drs"){
-      $url = "https://repository.library.northeastern.edu/api/v1/files/" . $neu_id;
+          $url = "https://repository.library.northeastern.edu/api/v1/files/" . $neu_id;
       $data = get_response($url);
       $data = json_decode($data);
 
@@ -295,9 +352,31 @@ function drstk_timeline( $atts ){
   $cache_time = 1000;
   set_transient(md5('PREFIX'.serialize($atts)) , $cache_output, $cache_time * 60);
   global $DRS_PLUGIN_URL;
-  wp_register_script('drstk_timelineCollection', $DRS_PLUGIN_URL . '/assets/js/timelineCollection.js');
-  wp_enqueue_script('drstk_timelineCollection');
-  wp_localize_script('drstk_timelineCollection','main_data',$main_data);
+
+    wp_register_script( 'drstk_timelineCollection', $DRS_PLUGIN_URL. '/assets/js/timelineCollection.js', array( 'jquery' ));
+    wp_enqueue_script('drstk_timelineCollection');
+
+    $reload_filtered_set_drs_nonce = wp_create_nonce( 'reload_filtered_set_drs' );
+
+    $timeline_nonce = wp_create_nonce( 'timeline_nonce' );
+
+    $timeline_obj = array(
+        'ajax_url' => admin_url('admin-ajax.php'),
+        'nonce'    => $timeline_nonce,
+        'home_url' => drstk_home_url()
+    );
+
+    $facets_info_data_obj = array(
+        'ajax_url' => admin_url('admin-ajax.php'),
+        'nonce' => $reload_filtered_set_drs_nonce,
+        "data"    => $facets_info_data,
+        'home_url' => drstk_home_url(),
+        "atts" => $atts,
+        "timeline_obj" => $timeline_obj
+    );
+
+    wp_localize_script('drstk_timelineCollection', 'facets_info_data_obj', $facets_info_data_obj );
+
   return $shortcode;
 }
 
@@ -318,7 +397,15 @@ function drstk_timeline_shortcode_scripts() {
         array( 'jquery' ));
         wp_enqueue_script('drstk_timelinepage');
 
+        $timeline_nonce = wp_create_nonce( 'timeline_nonce' );
+        $timeline_obj = array(
+            'ajax_url' => admin_url('admin-ajax.php'),
+            'nonce'    => $timeline_nonce,
+            'home_url' => drstk_home_url(),
+            'post_id' => $post->ID
+        );
 
+        wp_localize_script( 'drstk_timelinepage', 'timeline_obj', $timeline_obj );
 
 
 	}
