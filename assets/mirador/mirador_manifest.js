659cdf26bb19b93115173a76edd799c95ca7760d
Eli Zoller <zoller.eli@gmail.com>
2016-12-02 14:00:16 -0500

    better sample mirador js file

--- assets/mirador/mirador_manifest.js
+++ assets/mirador/mirador_manifest.js
@@ -4,13 +4,13 @@ $(function() {
    "id": "mirador_viewer",
    "layout": "1x1",
    "data": [
-      { "manifestUri": "[insert manifest.json URI like http://localhost/~beekerz/manifest.json]", "location":"Northeastern University Digital Repository Service (DRS)"},
+      { "manifestUri": "http://dragonprayerbook.northeastern.edu/manifest.json", "location":"Northeastern University Digital Repository Service (DRS)"},
    ],
    "windowObjects": [
           {
-          "loadedManifest" : "[insert manifest.json URI http://localhost/~beekerz/manifest.json]",
+          "loadedManifest" : "http://dragonprayerbook.northeastern.edu/manifest.json",
           "viewType" : "BookView"}
    ],
-   'buildPath' : '[insert path to install like /~beekerz/wordpress/wp-content/plugins/drs-tk/assets/mirador/]',
+   'buildPath' : '/wp-content/plugins/drs-tk/assets/mirador/',
  });
 });
