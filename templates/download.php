986d12c88a930db19737fa6fbd114d5808066503
David Cliff <d.cliff@neu.edu>
2018-11-09 16:07:46 -0500

    Trying redirect instead

--- templates/download.php
+++ templates/download.php
@@ -1,3 +1,8 @@
 <?php
-  $url = "https://repository.library.northeastern.edu/downloads/". $item_pid ."?datastream_id=content&token=" . drstk_drs_auth();
-  echo file_get_contents($url);
+  if($is_user_logged_in()){
+    $url = "https://repository.library.northeastern.edu/downloads/". $item_pid ."?datastream_id=content&token=" . drstk_drs_auth();
+    wp_redirect($url);
+    exit;
+  } else {
+    auth_redirect();
+  }
