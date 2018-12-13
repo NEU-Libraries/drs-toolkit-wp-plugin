a29bd4a2d1e302adcb37507228765113eee8f8bd
Patrick Murray-John <p.murray-john@northeastern.edu>
2018-12-13 16:31:23 -0500

    Make Collection/Collections on settings page into something more clear

--- drs-tk.php
+++ drs-tk.php
@@ -163,8 +163,8 @@ function register_drs_settings() {
   add_settings_field('drstk_facet_sort_order', 'Default Facet Sort', 'drstk_facet_sort_callback', 'drstk_options', 'drstk_facet_settings');
   register_setting('drstk_options', 'drstk_facet_sort_order');
 
-  add_settings_section('drstk_collections_settings', 'Collections Page', null, 'drstk_options');
-  add_settings_field('drstk_collections_page_title', 'Collections Page Title', 'drstk_collections_page_title_callback', 'drstk_options', 'drstk_collections_settings');
+  add_settings_section('drstk_collections_settings', 'Collections Browse Page', null, 'drstk_options');
+  add_settings_field('drstk_collections_page_title', 'Collections Browse Title', 'drstk_collections_page_title_callback', 'drstk_options', 'drstk_collections_settings');
   register_setting( 'drstk_options', 'drstk_collections_page_title' );
 
   add_settings_section('drstk_collection_settings', 'Collection Page', null, 'drstk_options');
@@ -675,13 +675,13 @@ function drstk_niec_metadata_title_callback(){
 
 function drstk_collections_page_title_callback(){
   echo '<input type="text" name="drstk_collections_page_title" value="';
-  if (get_option('drstk_collections_page_title') == ''){ echo 'Collections';} else { echo get_option('drstk_collections_page_title'); }
+  if (get_option('drstk_collections_page_title') == ''){ echo 'Browse Collections';} else { echo get_option('drstk_collections_page_title'); }
   echo '" />';
 }
 
 function drstk_collection_page_title_callback(){
   echo '<input type="text" name="drstk_collection_page_title" value="';
-  if (get_option('drstk_collection_page_title') == ''){ echo 'Browse';} else { echo get_option('drstk_collection_page_title'); }
+  if (get_option('drstk_collection_page_title') == ''){ echo 'Collection';} else { echo get_option('drstk_collection_page_title'); }
   echo '" />';
 }
 
