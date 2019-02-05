e6759a284af9bf45836447dec0f00c6f3aa2b743
patrickmj <p.murray-john@northeastern.edu>
2019-02-05 13:08:17 -0500

    curl setting for older PHP

--- drs-tk.php
+++ drs-tk.php
@@ -1046,6 +1046,7 @@ function get_response($url) {
   curl_setopt($ch, CURLOPT_FAILONERROR, false);
   $raw_response = curl_exec($ch);
   $response_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+//$response_status = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
   
   //fallback for PHP < 5.5
   // @TODO remove this once our servers are upgraded, so we can keep using modern(ish) PHP practices
