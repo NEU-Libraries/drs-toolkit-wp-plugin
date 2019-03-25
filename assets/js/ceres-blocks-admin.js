0f9516fb5b74eee7ca35f610a1ecf4023989780b
Patrick Murray-John <p.murray-john@northeastern.edu>
2019-03-25 12:49:10 -0400

    wireframe js for block search

---
+++ assets/js/ceres-blocks-admin.js
@@ -0,0 +1,33 @@
+jQuery(document).ready( function() {
+   console.log('ready');
+   jQuery('body').on('click', '.ceres.drs-search', function(e) {
+       const testHtml = 
+           '<ul>' +
+                '<li>' +
+                  '<label for="tile-neu:1033">' + 
+                  '<img src="https://repository.library.northeastern.edu/downloads/neu:3917?datastream_id=thumbnail_1">' +
+                  '<br>' +
+                  '<input id="tile-neu:1033" type="checkbox" class="tile drs" value="neu:1033">' +
+                  '<span class="title">Understanding contemporary maritime piracy</span>' +
+                  '</label>' +
+                '</li>' +
+                '<li>' +
+                '<label for="tile-neu:1033">' + 
+                '<img src="https://repository.library.northeastern.edu/downloads/neu:3917?datastream_id=thumbnail_1">' +
+                '<br>' +
+                '<input id="tile-neu:1033" type="checkbox" class="tile drs" value="neu:1033">' +
+                '<span class="title">Understanding contemporary maritime piracy</span>' +
+                '</label>' +
+              '</li>' +
+              '<li>' +
+              '<label for="tile-neu:1033">' + 
+              '<img src="https://repository.library.northeastern.edu/downloads/neu:3917?datastream_id=thumbnail_1">' +
+              '<br>' +
+              '<input id="tile-neu:1033" type="checkbox" class="tile drs" value="neu:1033">' +
+              '<span class="title">Understanding contemporary maritime piracy</span>' +
+              '</label>' +
+            '</li>' +
+            '</ul>';
+       jQuery('#ceres-search-results').html(testHtml);
+   })
+});
