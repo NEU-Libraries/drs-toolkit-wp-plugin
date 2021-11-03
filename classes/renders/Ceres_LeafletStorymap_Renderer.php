07eea17a83676b467d4c9fda935d018c164d4df9
Patrick Murray-John <p.murray-john@northeastern.edu>
2021-11-03 14:18:56 -0400

    reorganize classes and stub out anticipated ones

---
+++ classes/renders/Ceres_LeafletStorymap_Renderer.php
@@ -0,0 +1,8 @@
+<?php 
+
+class Ceres_LeafletStorymap_Renderer extends Ceres_Abstract_Renderer {
+  
+  public function render() {
+
+  }
+}
---
+++ classes/renders/Ceres_LeafletTimeline_Renderer.php
@@ -0,0 +1,8 @@
+<?php 
+
+class Ceres_LeafletTimeline_Renderer extends Ceres_Abstract_Renderer {
+  
+  public function render() {
+ 
+  }
+}
