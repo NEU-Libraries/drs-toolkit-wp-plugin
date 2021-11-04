dc73a964c07573eee64b7555ec0bbd66992d66f0
Patrick Murray-John <p.murray-john@northeastern.edu>
2021-11-04 09:50:37 -0400

    add stubs for pagination and facet renderers

---
+++ classes/renders/Ceres_Facets_Renderer.php
@@ -0,0 +1,8 @@
+<?php 
+
+class Ceres_Facets_Renderer extends Ceres_Abstract_Renderer {
+  
+  public function render() {
+  
+  }
+}
---
+++ classes/renders/Ceres_Pagination_Renderer.php
@@ -0,0 +1,8 @@
+<?php 
+
+class Ceres_Pagination_Renderer extends Ceres_Abstract_Renderer {
+  
+  public function render() {
+  
+  }
+}
