5f167a98e04ae80cf7c0eac74747b905f621c55a
eswarsaladi <99567868+eswarsaladi@users.noreply.github.com>
2022-07-08 04:49:38 -0400

    modified fetchfrom file to check allowedtypes

--- src/DRSApi/index.js
+++ src/DRSApi/index.js
@@ -2,16 +2,16 @@ import axios from "axios";
 /**
  * Parameters passed to fetch from File
  * @typedef  {Object} FetchFileParams
- * @property {string} format                - Type expected by block
- * @property {String|null} fileFormat       - Type in the data (Passed from Search)
+ * @property {Array<string>} allowedTypes   - Allowed types for the file returned
  * @property {string} fileId                - Id of the file to be retrieved
  */
 /**
  * Return type for fetch from File
  * @typedef  {Object} FetchFileResponse
- * @property {String} fileUrl               - Url of the File
+ * @property {String | undefined} fileUrl   - Url of the File
  * @property {String} fileId                - DRS Id of the File
- * @property {String} Format                - Format of the File
+ * @property {String} dataFormat            - Format of the File
+ * @property {String | undefined} err		- If error is present
  * @property {Object} mods                  - Details of the file
  */
 
@@ -20,29 +20,35 @@ import axios from "axios";
  * @param {FetchFileParams} params          - Data to be passed
  * @returns {Promise<FetchFileResponse>}    - Promise object
  */
-export async function fetchFromFile({ format, fileId, fileFormat = null }) {
+export async function fetchFromFile({ fileId, allowedTypes }) {
 	try {
-		let fileUrl = "";
+		let fileUrl;
+		let err;
 		const baseUrl = "https://repository.library.northeastern.edu/api/v1/files/";
 		const response = await axios.get(baseUrl + fileId);
 		const { data } = response;
 
-		if (format == "Image") {
-			if (fileFormat == null || fileFormat == "Image")
+		const dataFormat = data.mods.Format[0].toLowerCase();
+
+		if (allowedTypes.includes(dataFormat)) {
+			if (dataFormat === "image") {
+				// master image cannot be used, since the size is way too big!!!
 				Object.entries(data.content_objects).forEach(([key, value]) => {
+					// using large image
 					if (value.includes("Large")) {
 						fileUrl = key;
 					}
 				});
-			else {
-				const lenThumbnails = data.thumbnails.length;
-				fileUrl = data.thumbnails[lenThumbnails - 1];
+			} else {
+				// For everything else use cannonical object or master file
+				fileUrl = Object.keys(data.canonical_object)[0];
 			}
 		} else {
-			fileUrl = Object.keys(data.canonical_object)[0];
+			console.log("Data format not included in the allowedTypes");
+			err = "not in allowedTypes";
 		}
 
-		return { fileUrl, fileId, format, mods: data.mods };
+		return { fileUrl, fileId, dataFormat, mods: data.mods, err };
 	} catch (error) {
 		console.log(error);
 	}
