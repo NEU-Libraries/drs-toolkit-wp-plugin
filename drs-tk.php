b01f53789ac1125f9f3632e53086af07065fc46b
Eswar Saladi <eswar.saladi@ateliasoftware.com>
2022-08-15 11:25:06 -0400

    added settings page placeholder

--- drs-tk.php
+++ drs-tk.php
@@ -27,93 +27,23 @@ add_action(
     15
 );
 
-add_action('admin_menu', 'drstk_plugin_settings_page');
-/**
- * Registers the settings page
- */
-function drstk_plugin_settings_page()
+add_action('admin_menu', 'drstkSettingsPage');
+
+function drstkSettingsPage()
 {
-    add_menu_page(
-        __('Settings for CERES: Exhibit Toolkit Plugin', 'DRS Settings'),
-        __('CERES: Exhbit Toolkit', 'pre-publish-checklist'),
+    // title, menu title, permissions, slug, function
+    add_options_page(
+        'Drs Toolkit Settings',
+        'DRS Settings',
         'manage_options',
-        'drstk_admin_menu',
-        'drstk_display_settings',
-        'dashicons-yes'
+        'drs-tk-settings',
+        'drstkSettingsHtml'
     );
 }
 
-//this creates the form for the drstk settings page
-function drstk_display_settings()
+function drstkSettingsHtml()
 {
-    // I (PMJ) always prefer to build up all the HTML, but WP's settings_ stuff doesn't allow that. :(
     ?>
-        <div id="drstk-plugin-settings">
-            <?php esc_html_e('Requires JavaScript', 'pre-publish-checklist'); ?>
-        </div>
-        <?php
+    Hello World
+<?php
 }
-
-function drstk_plugin_register_settings()
-{
-    register_setting('drstk_plugin_settings', 'drstk_plugin_example_select', [
-        'default' => '',
-        'show_in_rest' => true,
-        'type' => 'string',
-    ]);
-
-    register_setting('drstk_plugin_settings', 'drstk_plugin_example_text', [
-        'default' => '',
-        'show_in_rest' => true,
-        'type' => 'string',
-    ]);
-
-    register_setting('drstk_plugin_settings', 'drstk_plugin_example_text_2', [
-        'default' => '',
-        'show_in_rest' => true,
-        'type' => 'string',
-    ]);
-
-    register_setting('drstk_plugin_settings', 'drstk_plugin_example_text_3', [
-        'default' => '',
-        'show_in_rest' => true,
-        'type' => 'string',
-    ]);
-
-    register_setting('drstk_plugin_settings', 'drstk_plugin_example_toggle', [
-        'default' => '',
-        'show_in_rest' => true,
-        'type' => 'string',
-    ]);
-}
-add_action('init', 'drstk_plugin_register_settings', 10);
-
-function drstk_plugin_admin_scripts()
-{
-    $dir = __DIR__;
-
-    $script_asset_path = "$dir/build/admin.asset.php";
-    if (!file_exists($script_asset_path)) {
-        throw new Error(
-            'You need to run `npm start` or `npm run build` for the "drs-tk" block first.'
-        );
-    }
-    $admin_js = 'build/admin.js';
-    $script_asset = require $script_asset_path;
-    wp_enqueue_script(
-        'drstk-plugin-admin-editor',
-        plugins_url($admin_js, __FILE__),
-        $script_asset['dependencies'],
-        $script_asset['version']
-    );
-    wp_set_script_translations('drstk-plugin-block-editor', 'drstk-plugin');
-
-    $admin_css = 'build/admin.css';
-    wp_enqueue_style(
-        'drstk-plugin-admin',
-        plugins_url($admin_css, __FILE__),
-        ['wp-components'],
-        filemtime("$dir/$admin_css")
-    );
-}
-add_action('admin_enqueue_scripts', 'drstk_plugin_admin_scripts', 10);
