dc8bfc262b1d28040dab14105c79799ca445848e
Patrick Murray-John <p.murray-john@northeastern.edu>
2022-11-16 09:01:10 -0500

    stubs out more of Abstract_Renderer based on current roadmap

--- classes/renderers/Ceres_Abstract_Renderer.php
+++ classes/renderers/Ceres_Abstract_Renderer.php
@@ -1,107 +1,146 @@
-<?php 
+  <?php 
 
-abstract class Ceres_Abstract_Renderer {
+  abstract class Ceres_Abstract_Renderer {
 
-  /**
-   * Options to pass along for the particular child renderers
-   * 
-   * @var array
-   */
+    /**
+     * Options to pass along for the particular child renderers
+     * 
+     * @var array
+     */
 
-  protected $options = array();
+    protected $options = array();
 
-  /**
-   * The running HTML to be returned by render()
-   * @TODO: often unused, so a candidate for removal.
-   * 
-   * @var string
-   */
+    /**
+     * The running HTML to be returned by render()
+     * @TODO: often unused, so a candidate for removal.
+     * 
+     * @var string
+     */
 
-  protected $html = '';
-  
-  /**
-   * The ID of the remote resource (DRS pid, DPLA hash id, etc)
-   * @var unknown
-   */
+    protected $html = '';
+    
+    /**
+     * The ID of the remote resource (DRS pid, DPLA hash id, etc)
+     * @var array or string
+     */
 
-  protected $resourceId;
+    protected $resourceId;
 
-  /**
-   * The Ceres_Abstract_Fetcher that are handling the data retrieval. Its itemData property
-   * holds the data to work with in most simple cases
-   * 
-   * Child classes' render() method will tell the Fetchers to, yaknow, fetch data
-   * as needed for it to put together the page (or page component).
-   * 
-   * @var array Ceres_Abstract_Fetcher
-   */
+    /**
+     * 
+     * The class used for the containing element for CSS customization purposes
+     * 
+     * @var string
+     */
+    protected $containerClass;
 
-  protected $fetchers = array();
-  
-  public function __construct(array $fetchers, $resourceId = null, $options = array()) {
-    foreach ($fetchers as $name => $class) {
-      $this->injectFetcher($name, $class);
+
+
+    /**
+     * The Ceres_Abstract_Fetcher(s) that are handling the data retrieval. Its itemData property
+     * holds the data to work with in most simple cases
+     * 
+     * Child classes' render() method will tell the Fetchers to, yaknow, fetch data
+     * as needed for it to put together the page (or page component).
+     * 
+     * @var array Ceres_Abstract_Fetcher
+     */
+
+    protected $fetchers = array();
+    
+    /**
+     * The Extractor(s) that will be used to wrangle raw responses from the Fetchers
+     * 
+     * extract() method on extractors will return just the needed data
+     * 
+     * @var array Ceres_Abstract_Extractor
+     * 
+     */
+
+    protected $extractors = array();
+
+    public function __construct(array $fetchers, array $extractors, $resourceId = null, $options = array()) {
+      foreach ($fetchers as $name => $class) {
+        $this->injectFetcher($name, $class);
+      }
+
+      foreach ($extractors as $name => $class) {
+        $this->injectExtractor($name, $class);
+      }
+
+
+      $this->setResourceId($resourceId);
+      $this->setOptions($options);
     }
-    $this->setResourceId($resourceId);
-    $this->setOptions($options);
-  }
 
-  abstract function render();
-  
-  // @TODO this might get moved into a separate Pagination Renderer, likely different for each Fetcher
-  //   First thought is that this'd just instantiate a new Renderer and tell it to do its thing
-  //   though that'd also mean injecting the relevant Fetcher into _that_ which might be 
-  //   getting crazy
-  abstract function buildPagination();
+    /* enqueing will have to figure out how to stuff styles and scripts in early in WP rendering.
+    Might have to go elsewhere */
 
-  public function setResourceId($resourceId) {
-    $this->resourceId = $resourceId;
-  }
+    function enqueStyles() {
 
-  public function getResourceId() {
-    return $this->resourceId;
-  }
-
-  public function setOptions(array $options) {
-    $this->options = $options;
-  }
-
-  /**
-   * Returns the entire options array
-   * 
-   * @return array
-   */
-
-  public function getOptions() {
-    return $this->options;
-  }
-
-  /**
-   * Set or unset an option value. Not passing a value unsets the option.
-   * 
-   * @param string $option
-   * @param string $value
-   */
-
-  public function setOption($option, $value = '') {
-    if ($value == '') {
-      unset($this->options[$option]);
-    } else {
-      $this->options[$option] = $value;
     }
-  }
 
-  public function getOption($option, $default = false) {
-    if (isset($this->options[$option])) {
-      return $this->options[$option];
+    function enqueScripts() {
+      
     }
-    return $default;
+    
+    abstract function render();
+    
+    // @TODO this might get moved into a separate Pagination Renderer, likely different for each Fetcher
+    //   First thought is that this'd just instantiate a new Renderer and tell it to do its thing
+    //   though that'd also mean injecting the relevant Fetcher into _that_ which might be 
+    //   getting crazy
+    
+    // abstract function buildPagination();
+
+    public function setResourceId($resourceId) {
+      $this->resourceId = $resourceId;
+    }
+
+    public function getResourceId() {
+      return $this->resourceId;
+    }
+
+    public function setOptions(array $options) {
+      $this->options = $options;
+    }
+
+    /**
+     * Returns the entire options array
+     * 
+     * @return array
+     */
+
+    public function getOptions() {
+      return $this->options;
+    }
+
+    /**
+     * Set or unset an option value. Not passing a value unsets the option.
+     * 
+     * @param string $option
+     * @param string $value
+     */
+
+    public function setOption($option, $value = '') {
+      if ($value == '') {
+        unset($this->options[$option]);
+      } else {
+        $this->options[$option] = $value;
+      }
+    }
+
+    public function getOption($option, $default = false) {
+      if (isset($this->options[$option])) {
+        return $this->options[$option];
+      }
+      return $default;
+    }
+    
+    
+    private function injectFetcher($name, $class) {
+      $this->fetchers[$name] = $class;
+    }
+    
+    
   }
-  
-  
-  private function injectFetcher($name, $class) {
-    $this->fetchers[$name] = $class;
-  }
-  
-  
-}
