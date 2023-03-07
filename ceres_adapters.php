ca9e97a91a107830f0d4f348a542f86fb0783b09
Patrick Murray-John <p.murray-john@northeastern.edu>
2023-03-07 17:59:36 -0500

    minor updates for the great marriage of CERES and WP

---
+++ ceres_adapters.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Tools here for connecting the broader WP/drs-tk environment to the 
+ * semi-independent CERES code under /libraries.
+ * 
+ * Future of this approach is uncertain as of 2023-03-07 10:59:40
+ * 
+ * 
+ * 
+ */
+
+use Ceres\ViewPackage\ViewPackage;
+
+/* LOAD CERES */
+
+require_once( plugin_dir_path( __FILE__ ) . '/libraries/Ceres/config/ceresSetup.php' );
+
+/* REGISTER SCRIPTS AND STYLES USED BY PARTICULAR VIEW PACKAGES */
+
+/* Registering them will make them available everywhere, but it looks
+*  like I can enqueue them from within a shortcode handler, so I'll do 
+*  it there based on need. 2023-03-07 17:58:38
+*/
+
+
+
+
+/* SETUP SHORTCODES USED BY CERES */
+
+add_shortcode('ceres_vp', 'ceres_vp_handler');
+add_shortcode('ceres_test', 'ceres_test_handler');
+
+/* DEFINE THE HANDLERS USED BY THE SHORTCODES */
+
+
+function ceres_vp_handler($atts) {
+
+    $atts = shortcode_atts(
+		array(
+			'vp_name' => '',
+			//'renderer_tableClass' => '',
+		),
+		$atts,
+		'ceres_vp'
+	);
+
+	wp_enqueue_script('jquery-ui-sortable');
+	$vp = new ViewPackage($atts['vp_name']);
+	$vp->build();
+	$vp->gatherData();
+	return $vp->render();
+
+}
+
+function ceres_test_handler($atts) {
+
+    $atts = shortcode_atts(
+		array(
+			'test_val' => 'Just testing!',
+		),
+		$atts,
+		'ceres_test'
+    );
+
+    return "<h3>" . $atts['test_val'] . "</h3>";
+	
+
+
+}
