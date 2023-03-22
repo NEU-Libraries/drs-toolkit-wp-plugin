02979c9bd0fbbf48520e522229f05b15dbf3aa5a
Patrick Murray-John <p.murray-john@northeastern.edu>
2023-03-22 18:44:42 -0400

    add tentative new shortcodes and drop independent leaflet js/css into WP enqueue system
    That'll need to be conditional someday
    part of #29
    part of #30

--- ceres_adapters.php
+++ ceres_adapters.php
@@ -23,16 +23,63 @@ require_once( plugin_dir_path( __FILE__ ) . '/libraries/Ceres/config/ceresSetup.
 *  it there based on need. 2023-03-07 17:58:38
 */
 
+/* Leaflet and Leaflet plugins */
+//@todo make enqueuing conditional
+wp_register_script('ceres_leaflet', plugins_url('/libraries/Ceres/assets/js/leaflet/leaflet1.7.1.js', __FILE__));
+wp_register_script('ceres_leaflet_wicket', plugins_url('/libraries/Ceres/assets/js/leaflet/brc/wicket-1.3.8.js', __FILE__));
+wp_register_script('ceres_leaflet_markercluster', plugins_url('/libraries/Ceres/assets/js/leaflet/leaflet-js-markercluster/leaflet.markercluster.js', __FILE__));
+wp_register_script('ceres_leaflet_bostonboundaries', plugins_url('/libraries/Ceres/assets/js/leaflet/brc/bostonboundaries.js', __FILE__));
+wp_register_script('ceres_leaflet_mask', plugins_url('/libraries/Ceres/assets/js/leaflet/leafet-plugin-mask.js', __FILE__));
+wp_register_script('ceres_leaflet_boundary-canvas', plugins_url('/libraries/Ceres/assets/js/leaflet/brc/leaflet-boundary-canvas.js', __FILE__));
+wp_register_script('ceres_leaflet_geolet', plugins_url('/libraries/Ceres/assets/js/leaflet/leaflet-plugin-geolet.js', __FILE__));
+wp_register_script('ceres_leaflet_fuse', plugins_url('/libraries/Ceres/assets/js/leaflet/fuse-leaflet-plugin-6-6-2.js', __FILE__));
+
+
+wp_register_style('ceres_leaflet', plugins_url('/libraries/Ceres/assets/css/leaflet/leaflet.css', __FILE__));
+wp_register_style('ceres_leaflet_brc-project', plugins_url('/libraries/Ceres/assets/css/leaflet/leaflet-brc-project.css', __FILE__));
+wp_register_style('ceres_leaflet_markercluster', plugins_url('/libraries/Ceres/assets/css/leaflet/leaflet-js-markercluster/MarkerCluster.css', __FILE__));
+wp_register_style('ceres_leaflet_markercluster_default', plugins_url('/libraries/Ceres/assets/css/leaflet/leaflet-js-markercluster/MarkerCluster.Default.css', __FILE__));
+
+
+//@todo  make enqueueing conditional upon the need
+wp_enqueue_script('ceres_leaflet');
+wp_enqueue_script('ceres_leaflet_wicket');
+wp_enqueue_script('ceres_leaflet_markercluster');
+wp_enqueue_script('ceres_leaflet_bostonboundaries');
+wp_enqueue_script('ceres_leaflet_mask');
+wp_enqueue_script('ceres_leaflet_boundary-canvas');
+wp_enqueue_script('ceres_leaflet_geolet');
+wp_enqueue_script('ceres_leaflet_fuse');
+
+wp_enqueue_style('ceres_leaflet');
+wp_enqueue_style('ceres_leaflet_brc-project');
+wp_enqueue_style('ceres_leaflet_markercluster');
+wp_enqueue_style('ceres_leaflet_markercluster_default');
 
 
 
-/* SETUP SHORTCODES USED BY CERES */
 
-add_shortcode('ceres_vp', 'ceres_vp_handler');
-add_shortcode('ceres_test', 'ceres_test_handler');
+
+// /* SETUP SHORTCODES USED BY CERES */
+
+// add_shortcode('ceres_vp', 'ceres_vp_handler');
+add_shortcode('ceres_renderer', 'ceres_renderer_handler');
+// add_shortcode('ceres_test', 'ceres_test_handler');
 
 /* DEFINE THE HANDLERS USED BY THE SHORTCODES */
 
+
+function ceres_renderer_handler($atts) {
+	$atts = shortcode_atts(
+		array(
+			'renderer' => '',
+		),
+		$atts,
+		'ceres_renderer'		
+	);
+	$rendererName = $atts['renderer'];
+	return "<h2>hi! I'll render from a $rendererName someday!</h2>";
+}
 
 function ceres_vp_handler($atts) {
 
@@ -45,7 +92,7 @@ function ceres_vp_handler($atts) {
 		'ceres_vp'
 	);
 
-	wp_enqueue_script('jquery-ui-sortable');
+	//wp_enqueue_script('jquery-ui-sortable');
 	$vp = new ViewPackage($atts['vp_name']);
 	$vp->build();
 	$vp->gatherData();
