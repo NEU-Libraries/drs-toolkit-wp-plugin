ed9ca0a528fda0ae961b61a22abac6439a56f3ca
Eswar Saladi <saladi.e@northeastern.edu>
2024-01-29 12:17:48 -0500

    added shortcode model

---
+++ assets/js/admin/models/shortcode.js
@@ -0,0 +1,45 @@
+import { Model } from 'backbone';
+import Items from '../collections/items';
+import Settings from '../collections/settings';
+import ColorSettings from '../collections/colorSettings';
+
+const Shortcode = Model.extend({
+    defaults: {
+        type: '',
+        items: new Items(),
+        settings: new Settings(),
+        colorsettings: new ColorSettings(),
+    },
+    // initialize the model
+    initialize: function () {
+        this.set('items', new Items());
+        this.set('settings', new Settings());
+        this.set('colorsettings', new ColorSettings());
+    },
+    // parse the response to get items and settings
+    parse: function (response) {
+        response.items = new Items(response.items);
+        response.settings = new Settings(response.settings);
+        response.colorsettings = new ColorSettings(response.colorsettings);
+        return response;
+    },
+    // set the model
+    set: function (attributes, options) {
+        const mappings = {
+            items: Items,
+            settings: Settings,
+            colorsettings: ColorSettings,
+        };
+
+        // If any of the attributes are not already a model, convert them to one
+        Object.entries(mappings).forEach(([key, Class]) => {
+            if (attributes[key] && !(attributes[key] instanceof Class)) {
+                attributes[key] = new Class(attributes[key]);
+            }
+        });
+
+        return Model.prototype.set.call(this, attributes, options);
+    },
+});
+
+export default Shortcode;
