5d768a71ce1667510a49cdfcd10f8558fd538bec
patrickmj <p.murray-john@northeastern.edu>
2025-04-10 14:49:33 -0400

    OOify public-ui

---
+++ public-ui/wrappers.php
@@ -0,0 +1,21 @@
+<?php
+
+use Drstk\PublicUI\Scripts;
+
+
+function drstk_item_script() {
+    Scripts::itemScript();
+
+}
+
+function drstk_breadcrumb_script() {
+    Scripts::breadcrumbScript();
+}
+
+function drstk_mirador_script() {
+    Scripts::miradorScript();
+}
+
+function drstk_browse_script() {
+    Scripts::browseScript();
+}
