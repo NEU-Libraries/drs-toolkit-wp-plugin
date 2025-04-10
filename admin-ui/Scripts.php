c500bc50e4e9837fb6e0a428abc9ce61d08b7cd6
patrickmj <p.murray-john@northeastern.edu>
2025-04-10 14:40:04 -0400

    OOify admin-ui

--- admin-ui/Scripts.php
+++ admin-ui/Scripts.php
@@ -1,22 +1,25 @@
 <?php
 
-//namespace Drstk\AdminUI;
+namespace Drstk\AdminUI;
 
-/*fix for weird jumpiness in wp admin menu*/
-function fix_admin_head() {
-	echo "<script type='text/javascript'>jQuery(window).load(function(){jQuery('#adminmenuwrap').hide().show(0);});</script>";
-    echo "<style>#start-pt-pb-tour{display:none !important;}</style>";
-}
+class Scripts {
 
-// admin_enqueue_scripts
+    /*fix for weird jumpiness in wp admin menu*/
+    // @todo is this still needed?
+    static function fixAdminHead(): void {
+        echo "<script type='text/javascript'>jQuery(window).load(function(){jQuery('#adminmenuwrap').hide().show(0);});</script>";
+        echo "<style>#start-pt-pb-tour{display:none !important;}</style>";
+    }
 
-function drstk_admin_enqueue() {
-    if (get_current_screen()->base == 'settings_page_drstk_admin_menu') {
-        // we are on the settings page
-        wp_enqueue_script('jquery-ui-sortable');
-        wp_register_script('drstk_meta_helper_js',
-            plugins_url('/assets/js/item_meta_helper.js', __FILE__),
-            array('jquery'));
-        wp_enqueue_script( 'drstk_meta_helper_js');
+    static function adminEnqueue(): void {
+        if (get_current_screen()->base == 'settings_page_drstk_admin_menu') {
+            // we are on the settings page
+            wp_enqueue_script('jquery-ui-sortable');
+            wp_register_script('drstk_meta_helper_js',
+                plugins_url('/assets/js/item_meta_helper.js', __FILE__),
+                array('jquery'));
+            wp_enqueue_script( 'drstk_meta_helper_js');
+        }
     }
 }
+
---
+++ admin-ui/wrappers.php
@@ -0,0 +1,125 @@
+<?php
+
+use Drstk\AdminUI\Actions;
+use Drstk\AdminUI\Filters;
+use Drstk\AdminUI\Scripts;
+
+// Actions
+
+
+
+function drstk_add_hypothesis() {
+    Actions::addHypothesis();
+}
+
+// INIT ACTIONS
+
+/* This makes it so that the tinymce wysiwyg does not process shortcodes so the database saves the shortcode before it is processed - this allows the has_shortcode function to work as expected and thus enqueue javascript correctly*/
+
+function remove_bstw_widget_text_filters() {
+    Actions::removeBstwWidgetTextFilters();
+}
+
+function register_drs_settings() {
+    Actions::registerDrsSettings();
+}
+
+function drstk_register_browse_settings(): void {
+    Actions::registerBrowseSettings();
+}
+
+function drstk_register_item_page_settings(): void {
+    Actions::registerItemPageSettings();
+}
+
+function drstk_register_search_settings(): void {
+    Actions::registerSearchSettings();
+}
+
+function drstk_register_project_settings(): void {
+    Actions::registerProjectSettings();
+}
+
+function drstk_register_podcast_settings(): void {
+    Actions::registerPodaseSettings();
+}
+
+function drstk_register_itunes_settings(): void {
+    Actions::registerItunesSettings();
+}
+
+function drstk_register_googleplay_settings() {
+    Actions::registerGooglePlaySettings();
+}
+
+function drstk_register_spotify_settings(): void {
+    Actions::registerSpotifySettings();
+}
+
+function drstk_register_stitcher_settings(): void {
+    Actions::registerStitcherSettings();
+}
+
+function drstk_register_overcast_settings(): void {
+    Actions::registerOvercastSettings();
+}
+
+function drstk_register_leaflet_settings(): void {
+    Actions::registerLeafletSettings();
+}
+
+function drstk_register_mirador_settings(): void {
+    Actions::registerMiradorSettings();
+}
+
+
+function drs_admin_add_page() {
+    Actions::adminAddPage();
+}
+function drstk_register_niec_settings(): void {
+    Actions::registerNiecSettings();
+}
+
+// Filters
+
+
+function drstk_image_attachment_fields_to_save($post, $attachment) {
+    return Filters::imageAttachmentFieldsToSave($post, $attachment);
+}
+
+
+function drstk_image_attachment_fields_to_edit($formFields, $post) {
+    return Filters::imageAttachmentFieldsToEdit($formFields, $post);
+}
+
+function drstk_podcast_page_template($template) {
+    return Filters::podcastPageTemplate($template);
+}
+
+
+function drstk_content_template($template) {
+    return Filters::contentTemplate($template);
+} 
+
+/**
+ * Register an additional query variable so we can differentiate between
+ * the types of custom queries that are generated
+ */
+
+function drstk_add_query_var($publicQueryVars) {
+    return Filters::addQueryVar($publicQueryVars);
+}
+
+// Scripts
+
+
+function fix_admin_head() {
+    Scripts::fixAdminHead();
+}
+
+
+function drstk_admin_enqueue() {
+    Scripts::adminEnqueue();
+}
+
+
